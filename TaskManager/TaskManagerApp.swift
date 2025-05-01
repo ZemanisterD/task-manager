8b823bced663aa232e238c9255ee191794e7b974
zemanisterD <vimanyu.sharma@dizartsolutions.com>
2025-05-01 13:16:55 +0530

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Zemanister Agues on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Zemanister Agues on 01/05/25.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
